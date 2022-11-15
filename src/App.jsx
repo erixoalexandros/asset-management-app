574e998e9e76efd09499750724e34fd7b29a6456
Jordan <ejordan3@CougarNet.UH.EDU>
2022-11-15 16:09:59 -0600

    Add Folder Structure and Some App Config

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <h1 className="bg-blue-400 text-white w-fit mx-auto py-2 px-4 text-center">
         Asset Management App
       </h1>
-    </>
+      <Routes>
+        <Route exact path="/" element={<h2>Home</h2>} />
+        <Route
+          path="/asset"
+          element={
+            <>
+              <h2>Asset Name</h2>
+              <Link to="/">Home</Link>
+            </>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
