3a5aaa6d8f669bbe914e0df35ebef7eb7be296fc
Jordan <ejordan3@CougarNet.UH.EDU>
2022-11-15 15:11:46 -0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="bg-blue-400 text-white w-fit mx-auto py-2 px-4 text-center">
+        Asset Management App
+      </h1>
+    </>
+  );
+}
+
+export default App;
